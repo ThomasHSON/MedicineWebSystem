51a2cc94da9ad952f8d7730a56e36319435284fa
LeonLu3303 <leon.lu1103@gmail.com>
2025-10-27 16:37:21 +0800

    asdfasdf

--- ApiURL.js
+++ ApiURL.js
@@ -1,10 +1,10 @@
 var flag_check_connection_OK = false;
 var APIServer = [];
 
-var ipadress1 = 'http://www.ketech.tw:4435';
-var ipadress2 = 'http://www.ketech.tw:4436';
+var ipadress1 = "http://www.ketech.tw:4435";
+var ipadress2 = "http://www.ketech.tw:4436";
 
-var api_ip = `${ipadress1}/`; 
+var api_ip = `${ipadress1}/`;
 var Chat_url = `${api_ip}chatHub`;
 var MessageAPI_url = `${api_ip}api/Message`;
 var inspection_url = `${api_ip}api/inspection`;
@@ -15,174 +15,153 @@ var device_url = `${api_ip}api/device`;
 var session_url = `${api_ip}api/session`;
 var session_login_url = ``;
 var MED_page_url = ``;
-async function LoadAPIServer(log)
-{
+async function LoadAPIServer(log) {
   const json = await Loadtxt("../../config.txt");
   console.log("抓到config.txt檔案資料=====>", json);
   json.API_Server = is_https_trans_ip(json.API_Server);
   APIServer = await getDataFromAPI(`${json.API_Server}/api/ServerSetting`);
   console.log("APIServer 抓到的資料", APIServer);
-  const API_Session = serch_APIServer("Main","網頁" ,"API_Session");
+  const API_Session = serch_APIServer("Main", "網頁", "API_Session");
   session_url = `${is_https_trans_ip(API_Session[0].server)}/api/session`;
-  if(!log)console.log("session_url",session_url);
+  if (!log) console.log("session_url", session_url);
 
-  const API_Session_Login = serch_APIServer("Main","網頁" ,"API_Login");
+  const API_Session_Login = serch_APIServer("Main", "網頁", "API_Login");
 
   // session_login_url = `${API_Session_Login[0].server}`;
   // console.log("session_login_url",session_login_url);
 
   session_login_url = `${is_https_trans_ip(API_Session_Login[0].server)}`;
-  // console.log("session_login_url",session_login_url);
+  console.log("session_login_url", session_login_url);
 
-  if(!log)console.log("session_login_url",session_login_url);
+  if (!log) console.log("session_login_url", session_login_url);
   return APIServer;
 }
-function serch_APIServer(name ,type, content)
-{
+function serch_APIServer(name, type, content) {
   var temp = APIServer.Data;
-  if(name != "") temp  = searchJSON(temp,"name",name);
-  if(type != "") temp  = searchJSON(temp,"type",type);
-  if(content != "") temp  = searchJSON(temp,"content",content);
+  if (name != "") temp = searchJSON(temp, "name", name);
+  if (type != "") temp = searchJSON(temp, "type", type);
+  if (content != "") temp = searchJSON(temp, "content", content);
   return temp;
 }
-async function check_ip(ip0 , ip1)
-{
-    ip0 = is_https_trans_ip(ip0);
-    ip1 = is_https_trans_ip(ip1);
+async function check_ip(ip0, ip1) {
+  ip0 = is_https_trans_ip(ip0);
+  ip1 = is_https_trans_ip(ip1);
 
-    if(flag_check_connection_OK) 
-    {
-        console.log('flag_check_connection_OK',flag_check_connection_OK);
-        return;
-    }
-    var api_ip_temp;
-    if(await pingIP(ip0))
-    {
-        api_ip_temp = `${ip0}/`;     
-        flag_check_connection_OK = true;
-    }
-    else if(await pingIP(ip1))
-    {
-        api_ip_temp = `${ip1}/`;     
-        flag_check_connection_OK = true;
-    }
-    if(flag_check_connection_OK)
-    {
-        console.log("成功設定IP : " ,api_ip_temp);
-        api_ip = api_ip_temp;
-        api_ip = is_https_trans_ip(api_ip);
-        console.log("成功導向https_IP : " ,api_ip);
-        inventory_url = `${api_ip_temp}api/inventory`;
-        inspection_url = `${api_ip_temp}api/inspection`;
-        transactions_url = `${api_ip_temp}api/transactions`;
-        device_url = `${api_ip_temp}api/device`;
-        consumption_url = `${api_ip_temp}api/consumption`;
-        MED_page_url = `${api_ip_temp}api/MED_page`
-        return;
-    }
-    check_ip(ip0 ,ip1);
+  if (flag_check_connection_OK) {
+    console.log("flag_check_connection_OK", flag_check_connection_OK);
+    return;
+  }
+  var api_ip_temp;
+  if (await pingIP(ip0)) {
+    api_ip_temp = `${ip0}/`;
+    flag_check_connection_OK = true;
+  } else if (await pingIP(ip1)) {
+    api_ip_temp = `${ip1}/`;
+    flag_check_connection_OK = true;
+  }
+  if (flag_check_connection_OK) {
+    console.log("成功設定IP : ", api_ip_temp);
+    api_ip = api_ip_temp;
+    api_ip = is_https_trans_ip(api_ip);
+    console.log("成功導向https_IP : ", api_ip);
+    inventory_url = `${api_ip_temp}api/inventory`;
+    inspection_url = `${api_ip_temp}api/inspection`;
+    transactions_url = `${api_ip_temp}api/transactions`;
+    device_url = `${api_ip_temp}api/device`;
+    consumption_url = `${api_ip_temp}api/consumption`;
+    MED_page_url = `${api_ip_temp}api/MED_page`;
+    return;
+  }
+  check_ip(ip0, ip1);
 }
-async function get_url(url)
-{
-    return api_ip;
+async function get_url(url) {
+  return api_ip;
 }
-async function set_ip(flag_api_server)
-{
-    if(flag_api_server == null ) await LoadAPIServer();
-    check_ip(ipadress1 ,ipadress2);
+async function set_ip(flag_api_server) {
+  if (flag_api_server == null) await LoadAPIServer();
+  check_ip(ipadress1, ipadress2);
 }
 
-async function Set_ChatHub_url()
-{
-    console.log("Set_ChatHub_url");
-    await LoadAPIServer(true);
-    var flag_OK = false;
-    const json = await Loadtxt("../../config.txt");
-    json.API_Server = is_https_trans_ip(json.API_Server);
+async function Set_ChatHub_url() {
+  console.log("Set_ChatHub_url");
+  await LoadAPIServer(true);
+  var flag_OK = false;
+  const json = await Loadtxt("../../config.txt");
+  json.API_Server = is_https_trans_ip(json.API_Server);
 
-    const json_return = await getDataFromAPI(`${json.API_Server}/api/ServerSetting`);
-    const json_API_Server = searchJSON(json_return.Data,"type","網頁");
-    const API01 = searchJSON(json_API_Server,"content","API01");
-    const API02 = searchJSON(json_API_Server,"content","API02");
-    var api_ip_temp;
-    if(await pingIP(API01[0].server))
-    {
-        api_ip_temp = `${is_https_trans_ip(API01[0].server)}/`;     
-        flag_OK = true;
-    }
-    else if(await pingIP(API02[0].server))
-    {
-        api_ip_temp = `${is_https_trans_ip(API02[0].server)}/`;     
-        flag_OK = true;
-    }
-    if(flag_OK)
-    {
-        Chat_url = `${api_ip_temp}chatHub`;
-        MessageAPI_url = `${api_ip_temp}api/Message`;
-    }
+  const json_return = await getDataFromAPI(
+    `${json.API_Server}/api/ServerSetting`
+  );
+  const json_API_Server = searchJSON(json_return.Data, "type", "網頁");
+  const API01 = searchJSON(json_API_Server, "content", "API01");
+  const API02 = searchJSON(json_API_Server, "content", "API02");
+  var api_ip_temp;
+  if (await pingIP(API01[0].server)) {
+    api_ip_temp = `${is_https_trans_ip(API01[0].server)}/`;
+    flag_OK = true;
+  } else if (await pingIP(API02[0].server)) {
+    api_ip_temp = `${is_https_trans_ip(API02[0].server)}/`;
+    flag_OK = true;
+  }
+  if (flag_OK) {
+    Chat_url = `${api_ip_temp}chatHub`;
+    MessageAPI_url = `${api_ip_temp}api/Message`;
+  }
 }
 
-async function pingIP(ipAddress, timeout = 100000) 
-{
-    ipAddress = is_https_trans_ip(ipAddress);
-    const url = `${ipAddress}/api/test`;
-    console.log("pingIP", url);
-  
-    const controller = new AbortController();
-    const signal = controller.signal;
-  
-    const timeoutId = setTimeout(() => {
-      controller.abort();
-    }, timeout);
-  
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        signal: signal
-      });
-  
-      clearTimeout(timeoutId);
-  
-      if (response.ok) {
-        // console.log(response);
-        return true;
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    } catch (error) {
-      console.error(`PING 連結失敗! ${ipAddress}`, error);
-      return false;
+async function pingIP(ipAddress, timeout = 100000) {
+  ipAddress = is_https_trans_ip(ipAddress);
+  const url = `${ipAddress}/api/test`;
+  console.log("pingIP", url);
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const timeoutId = setTimeout(() => {
+    controller.abort();
+  }, timeout);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      signal: signal,
+    });
+
+    clearTimeout(timeoutId);
+
+    if (response.ok) {
+      // console.log(response);
+      return true;
+    } else {
+      throw new Error("Network response was not ok.");
     }
+  } catch (error) {
+    console.error(`PING 連結失敗! ${ipAddress}`, error);
+    return false;
+  }
 }
 
-
-
-const Enum_Target =
-{
-    Dept : 'Dept',
-    Phar : 'Phar',
-}
+const Enum_Target = {
+  Dept: "Dept",
+  Phar: "Phar",
+};
 var Target = Enum_Target.Dept;
 
-const Enum_BasicDeviceTableName = 
-{
-    Dept: 'firstclass_device_jsonstring',
-    Phar: 'phar_device_jsonstring',
-    SMDS: 'devicebasic_jsonstring'
+const Enum_BasicDeviceTableName = {
+  Dept: "firstclass_device_jsonstring",
+  Phar: "phar_device_jsonstring",
+  SMDS: "devicebasic_jsonstring",
 };
 
 var BalsicDeviceTableName = "";
-if(Target == Enum_Target.Dept)
-{
-    BalsicDeviceTableName = Enum_BasicDeviceTableName.Dept;
-}
-else if(Target == Enum_Target.Phar)
-{
-    BalsicDeviceTableName = Enum_BasicDeviceTableName.Phar;
+if (Target == Enum_Target.Dept) {
+  BalsicDeviceTableName = Enum_BasicDeviceTableName.Dept;
+} else if (Target == Enum_Target.Phar) {
+  BalsicDeviceTableName = Enum_BasicDeviceTableName.Phar;
 }
 function is_https_trans_ip(ip) {
   console.log(ip);
@@ -196,24 +175,27 @@ function is_https_trans_ip(ip) {
   console.log("https", temp_str.includes("s"));
   if (temp_str.includes("s")) {
     console.log("目前的連線是 HTTPS");
-    if (!temp_url.startsWith("https")) {     
+    if (!temp_url.startsWith("https")) {
       let newStr = temp_url.replace("http", "https");
       // 取出 port 號
-  
+
       let portRegex = /:(\d+)(\/|$)/;
       let match = newStr.match(portRegex);
-  
+
       if (match) {
-          // 提取並轉換 port 號為整數
-          let originalPort = parseInt(match[1]);
-          let newPort = originalPort + 10;
-  
-          // 替換原始的 port 號為新的 port 號
-          newStr =  newStr.replace(portRegex, `:${newPort}$2`);
-          console.log(newStr);
-          newStr = newStr.replace(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/, domain);
+        // 提取並轉換 port 號為整數
+        let originalPort = parseInt(match[1]);
+        let newPort = originalPort + 10;
+
+        // 替換原始的 port 號為新的 port 號
+        newStr = newStr.replace(portRegex, `:${newPort}$2`);
+        console.log(newStr);
+        newStr = newStr.replace(
+          /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/,
+          domain
+        );
       }
-      
+
       temp_url = newStr;
     }
   } else {
@@ -224,7 +206,3 @@ function is_https_trans_ip(ip) {
   return temp_url;
   // return close_url;
 }
-
-
-
-
